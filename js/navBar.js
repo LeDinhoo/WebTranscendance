837562eb17b560d021ef64d7e3475ef0dee6f7d4
LeDinhoo <darksheep42350@gmail.com>
2024-10-04 13:35:43 +0200

    navBar

--- js/navBar.js
+++ js/navBar.js
@@ -1,8 +1,23 @@
 const labels = document.querySelectorAll(".navLabel");
+const icons = document.querySelectorAll(".iconMenu");
 
 labels.forEach((label, index) => {
-  console.log(label.textContent);
-    if (index !== 0) {
-        label.style.display = "none";
-    }
+  if (index === 0) {
+    label.classList.add('active');
+  } else {
+    label.classList.remove('active');
+  }
+});
+
+icons.forEach((icon, index) => {
+  icon.addEventListener("click", () => {
+    console.log("clicked");
+    labels.forEach((label, i) => {
+      if (index === i) {
+        label.classList.add('active');
+      } else {
+        label.classList.remove('active');
+      }
+    });
+  });
 });
